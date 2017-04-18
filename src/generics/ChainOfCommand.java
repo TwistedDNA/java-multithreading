7fe969e4862b2f321054a5080730e75fa479b4de
TwistedDNA <maksym.dna.mazur@gmail.com>
2017-04-18 19:26:43 +0200

    setting task for generic type checking

---
+++ src/generics/ChainOfCommand.java
@@ -0,0 +1,14 @@
+package generics;
+
+/**
+ * Created by Lx on 4/18/2017.
+ */
+public class ChainOfCommand {
+
+    public static void main(String[] args) {
+        //TODO
+        // whenOfType(String.class).then(System.out::println)
+        //.whenOfType(Integer.class).then(Integer::toString)
+        //.execute();
+    }
+}
