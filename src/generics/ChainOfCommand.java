019f738ac8f93c00d4090e60c14d25bf0316e8f6
mmazur <maksym_mazur@epam.com>
2017-04-19 11:49:30 +0200

    ChainOfCommand task decription and part of sollution

--- src/generics/ChainOfCommand.java
+++ src/generics/ChainOfCommand.java
@@ -1,14 +1,42 @@
 package generics;
 
+import java.util.function.Consumer;
+
 /**
  * Created by Lx on 4/18/2017.
  */
 public class ChainOfCommand {
 
+    private Object obj;
+
+    public ChainOfCommand(Object obj) {
+        this.obj = obj;
+    }
+
     public static void main(String[] args) {
-        //TODO
-        // whenOfType(String.class).then(System.out::println)
-        //.whenOfType(Integer.class).then(Integer::toString)
-        //.execute();
+        /*int result = whenTypeOf(obj)
+            .is(String.class).thenReturn(String::length)
+            .is(Integer.class).thenReturn(d -> d)
+        execute();*/
+    }
+
+    public static ChainOfCommand whenTypeOf(Object obj) {
+        return new ChainOfCommand(obj);
+    }
+
+    public <T> T is(Class<T> clazz){
+        return new Typed(T);
+    }
+
+    class Typed<T> {
+        private T type;
+
+        public Typed(T type) {
+            this.type = type;
+        }
+
+        public  thenReturn(Consumer consumer){
+
+        }
     }
 }
